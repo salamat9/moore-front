95c429644263d6e0873733730eb25601b6d95e6a
Salamat Baichoroev <salamat@Salamats-MacBook-Pro.local>
2023-11-21 16:46:34 +0600

    http

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import './styles/App.scss';
 import {AuthContext} from './context';
 import {useEffect,useState} from 'react';
 import AppRouter from './components/AppRouter';
+import { getAccessToken } from './helpers/user';
 
 function App() {
   const [isAuth,setIsAuth] = useState(false)
   const [isLoading,setIsLoading] = useState(true)
 
   useEffect(() => {
-    if (localStorage.getItem('auth')) {
+    if (getAccessToken()) {
       setIsAuth(true);
-    };
+    }
     setIsLoading(false);
-  },[]);
+  }, []);
 
   return (
     <AuthContext.Provider value={{
