40094e57cb2506229493d5274eab3b5a60e96251
Azim <azimreact@gmail.com>
2023-11-21 19:59:27 +0600

    apartments page

---
+++ src/components/Table/MyTable.jsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { ReactComponent as MySvgIcon } from '../../assets/icons/points.svg';
+
+import './styles.scss'
+
+export default function BasicTable({data, setModal}) {
+  console.log('apartm', data);
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow className='table-header'>
+            <TableCell align="left">Квартиры</TableCell>
+            <TableCell align="left">Объект</TableCell>
+            <TableCell align="left">Этаж</TableCell>
+            <TableCell align="left">КВ</TableCell>
+            <TableCell align="left">Дата</TableCell>
+            <TableCell align="left">Статус</TableCell>
+            <TableCell align="left">Цена</TableCell>
+            <TableCell align="left">Клиент</TableCell>
+            <TableCell align="left">Бронь</TableCell>
+            <TableCell align="left"></TableCell>
+            <TableCell align="left"></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data ? data.data.map((data) => (
+            <TableRow
+              className='table-header'
+              key={data.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="left">{data.number}</TableCell>
+              <TableCell align="left">{data.description}</TableCell>
+              <TableCell align="left">{data.floor}</TableCell>
+              <TableCell align="left">{data.square}</TableCell>
+              <TableCell align="left">{data.createdAt?.slice(0, 11)}</TableCell>
+              <TableCell align="left">{data.status}</TableCell>
+              <TableCell align="left">{data.price}</TableCell>
+              <TableCell align="left">{data.clients}</TableCell>
+              <TableCell align="left">{data.updatedAt}</TableCell>
+              <TableCell align="left"><div className='change'>Изменить</div></TableCell>
+              <TableCell align="left"><MySvgIcon onClick={() => setModal(true)}/></TableCell>
+            </TableRow>
+          )) : ''}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- src/pages/Flats.jsx
+++ src/pages/Flats.jsx
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import PageWrapper from '../layouts/PageWrapper';
-import BasicTable from '../components/MyTable';
+import BasicTable from '../components/Table/MyTable';
+import {getApartments} from '../http/api/managers';
+import MyModal from '../components/UI/myModal/MyModal';
+import MyButton from '../components/UI/button/MyButton';
 
 const Flats = () => {
+  const [apartm, setApart] = useState();
+  const [modal, setModal] = useState(false);
+  
+  const getData = async () => {
+    return await getApartments();
+  }
+  
+  useEffect(()=> {
+    const fetchData = async () => {
+      const data = await getData();
+      setApart(data);
+    };
+    fetchData();
+  },[])
+  
+  useEffect(() => {
+    console.log('apartm', apartm);
+  }, [apartm])
   
   return (
     <PageWrapper>
+    <MyModal visible={modal} setVisible={setModal}>
+      <div>
+        <div>Вы действительно хотите удалить менеджера?</div>
+        <div className='btns'> 
+          <MyButton>Да</MyButton>
+          <MyButton onClick={() => setModal(false)}>Нет</MyButton>
+        </div>
+      </div>
+    </MyModal>
       <div className='header'>Квартиры</div>
       <hr />
       <div className='content-container'>
-        
-        <BasicTable />
+        <BasicTable setModal={setModal} data={apartm}/>
       </div>
     </PageWrapper>
   );
